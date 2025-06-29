a0d950416b7f145a5f8905cd1e39e31fc76acaa3
bhavanareddyd <bhavanareddyd.12@gmail.com>
2025-06-29 11:36:31 +0530

    chnaged backport number to 8000

--- app.py
+++ app.py
@@ -41,5 +41,4 @@ def predict():
     return jsonify({'prediction': label})
 
 if __name__ == '__main__':
-    os.makedirs("uploads", exist_ok=True)
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=8000)
